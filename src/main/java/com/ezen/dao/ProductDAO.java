b36e384e607137129f0a74fc5edefe6f5aa185e3
yeonju25 <129915263+yeonju25@users.noreply.github.com>
2023-06-27 12:13:23 +0900

    Update ProductDAO.java

--- src/main/java/com/ezen/dao/ProductDAO.java
+++ src/main/java/com/ezen/dao/ProductDAO.java
@@ -26,13 +26,13 @@ public class ProductDAO {
 	}
 	
 	// 데이터 크롤링해서 list에 받아서 return, list에 저장하면서 DB에도 저장
-	public List<ProductVO> productAllSelect() {	
+	public List<ProductVO> productAllAdd() {	
 		
 		List<ProductVO> list = new ArrayList<ProductVO>();
 		
 		Connection conn = null;
 		PreparedStatement pstmt = null;
-		String sql = "insert into productlist values (?,?,?,?)";
+		String sql = "insert into productlist values (?,?,?,?,?,?)";
 		
 		try {
 			int k = 1;
@@ -41,6 +41,7 @@ public class ProductDAO {
 				Elements images = doc.select("img.MS_prod_img_l");
 				Elements name = doc.select("span.goods_name");
 				Elements price = doc.select("span.sale_price");
+				Elements link = doc.select("ul.goodsDisplayTextWrap li a");
 				
 				for(int j=0; j<images.size(); j++) {
 					try {
@@ -48,13 +49,22 @@ public class ProductDAO {
 //						System.out.println("images : "	+ images.get(j).attr("src"));
 //						System.out.println("name : " + name.get(j).text());
 //						System.out.println("price : " + price.get(j).text());
-						
-						
+
+						// 각 상품당 클릭시 상세페이지로 넘어가는 링크
+						Document doc2 = Jsoup.connect("http://www.theskinfood.com"+link.get(j).attr("href")).get();
+		                  
+						// 상세페이지에서 태그 긁어오기
+		                Elements tag = doc2.select("span.product-info__item-content--tag a");
+		                
+		                int review = (int)(Math.random()*300+1); 
+		                
 						ProductVO vo = new ProductVO();
 						vo.setCode(k);
-						vo.setImage(images.get(j).attr("src"));
+						vo.setImage("https://www.theskinfood.com/" + images.get(j).attr("src"));
 						vo.setName(name.get(j).text());
 						vo.setPrice(price.get(j).text().substring(2).replace(",", "")); // DB에 number로 넣기 위해 통화기호랑 , 잘라내기
+						vo.setTag(tag.text());
+						vo.setReview(review);
 						
 						list.add(vo);
 						
@@ -65,6 +75,8 @@ public class ProductDAO {
 						pstmt.setString(2, vo.getName());
 						pstmt.setInt(3, Integer.parseInt(vo.getPrice()));
 						pstmt.setString(4, vo.getImage());
+						pstmt.setString(5, " " + vo.getTag());
+						pstmt.setInt(6, vo.getReview());
 						
 						pstmt.executeUpdate();
 					
@@ -87,6 +99,45 @@ public class ProductDAO {
 		return list;
 	}
 	
+	// 데이터 전부 선택
+	public List<ProductVO> productAllSelect() {
+		List<ProductVO> list = new ArrayList<ProductVO>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select * from productlist order by id";
+		
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				ProductVO vo = new ProductVO();
+				vo.setCode(rs.getInt("id"));
+				vo.setName(rs.getString("name"));
+				vo.setPrice(""+rs.getInt("price"));
+				vo.setImage(rs.getString("image"));
+				vo.setTag(rs.getString("tag"));
+				vo.setReview(rs.getInt("review"));
+				
+				list.add(vo);
+			}
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			try {
+				DBManager.close(conn, pstmt, rs);
+			}catch(Exception e) {
+				e.printStackTrace();
+			}
+		}
+		return list;
+	}
+	
+	
 	
 	// 데이터 1건 추출
 	public ProductVO selectOneProductByNum(int num) {
@@ -95,7 +146,7 @@ public class ProductDAO {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-		String sql = "select * from productlist where num = ?";
+		String sql = "select * from productlist where id = ?";
 		
 		try {
 			
@@ -110,6 +161,8 @@ public class ProductDAO {
 				vo.setName(rs.getString("name"));
 				vo.setPrice(""+rs.getInt("price"));
 				vo.setImage(rs.getString("image"));
+				vo.setTag(rs.getString("tag").replace(" ", " #"));
+				vo.setReview(rs.getInt("review"));
 			}
 			
 		}catch(Exception e) {
@@ -121,4 +174,26 @@ public class ProductDAO {
 		return vo;
 	}
 	
-}
+	//데이터 개수 가져오기
+	public int countProduct() {
+		int count = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		String sql = "select count(*) from productlist";
+		
+		try {
+			conn = DBManager.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			if(rs.next()) {
+				count = rs.getInt("count");
+			}
+		}catch(Exception e) {
+			e.printStackTrace();
+		}finally {
+			DBManager.close(conn, pstmt, rs);
+		}
+		return count;
+	}
