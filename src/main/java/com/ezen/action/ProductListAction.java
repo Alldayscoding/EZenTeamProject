a905b587d75a4964828f13af798591e15a2b3a30
yeonju25 <129915263+yeonju25@users.noreply.github.com>
2023-06-27 12:14:11 +0900

    Update ProductListAction.java

--- src/main/java/com/ezen/action/ProductListAction.java
+++ src/main/java/com/ezen/action/ProductListAction.java
@@ -18,16 +18,26 @@ public class ProductListAction implements Action{
 	public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		ProductDAO pDao = ProductDAO.getInstance();
 		List<ProductVO> list = pDao.productAllSelect();
+		
+		// 데이터가 있으면 list에 DB데이터 담기, DB에 저장된 데이터가 없으면 크롤링한 내용을 DB에 저장하고 list에 담아 화면으로 전달
+		if(list.size() == 0) {
+			list = pDao.productAllAdd();
+		}
 		request.setAttribute("productList", list);
+		
+		// 페이징
+		int page = 1; // 시작 페이지
+
+		int itemsInAPage = 20;						// 한 페이지에 출력할 게시물 수
+		int totalCount = pDao.countProduct();		// DB에서 전체 게시물 카운트
+		int totalPage = (int) Math.ceil(totalCount / (double) itemsInAPage);
+		request.setAttribute("totalCount", totalCount);
+		request.setAttribute("totalPage", totalPage);
+		request.setAttribute("page", page);
+		
 		
 		String url = "/product/productList.jsp";
 		RequestDispatcher dis = request.getRequestDispatcher(url);
 		dis.forward(request, response);
 	}
-
-	
-	
 }	
-
-
-
